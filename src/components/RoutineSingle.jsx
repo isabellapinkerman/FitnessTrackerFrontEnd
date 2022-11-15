620c381a5966b7635194efddf9ff69c09e747a7f
Robby Bacus <robbybacus@gmail.com>
2022-11-15 06:17:56 -0500

    css org for routines & activities

--- src/components/RoutineSingle.jsx
+++ src/components/RoutineSingle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RoutineSingle = (props) => {
   const routine = props.routine;
-  console.log(routine);
+  console.log(routine.activities);
 
   return (
     <>
@@ -10,18 +10,26 @@ const RoutineSingle = (props) => {
         <div>{routine.id}</div>
         <div>{routine.name}</div>
         <div>{routine.goal}</div>
-        <div>{routine.activities.length ? (
-          routine.activities.map((activity) => {
-            return (
-              <div
-                key={`activity-${`THIS IS EMPTY RIGHT NOW`}`}
-                routine={ routine }
-              >{`${activity}`}</div>
-            );
-          })
-        ) : (
-          <div>Loading Routines</div>
-        )}</div>
+        <div className="activities">
+          {routine.activities.length ? (
+            routine.activities.map((activity) => {
+              return (
+                <div className = "activity"
+                  key={`activity-${activity.id}`}
+                  routine={routine}
+                >
+                  <div>{`ID: ${activity.id}`}</div>
+                  <div>{`Name: ${activity.name}`}</div>
+                  <div>{`Description: ${activity.description}`}</div>
+                  <div>{`Duration: ${activity.duration}`}</div>
+                  <div>{`Count: ${activity.count}`}</div>
+                </div>
+              );
+            })
+          ) : (
+            <div>Loading Routines</div>
+          )}
+        </div>
       </div>
     </>
   );

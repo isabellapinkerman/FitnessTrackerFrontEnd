e0bbe6db2c1bd92c06340abfec63bd41b90a8be4
Bella <Isabellapinkerman@gmail.com>
2022-11-17 15:09:14 -0500

    activities css and home page styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,19 @@ const Home = () => {
       <div className="homeHeader">
         Organize and schedule your Fitness Journey with Fitness Tracker!
       </div>
-      <div>Get started Today!</div>
       <div>
+      <div className="today">Get started Today!</div>
+      <div className="homeBody">
         <div>
           Benefits
           <p>Create Workout Routines for you and your friends</p>
           <p>Track and share your Fitness Journey</p>
           <p>An organized schedule to help Motivate and Inspire</p>
         </div>
+        <div className="highFiveImg">
+          <img src="https://t3.ftcdn.net/jpg/03/08/72/80/240_F_308728099_AdWDcjBTazzfGCAvb54HuWmaTmbd9Xi9.jpg"></img>
+        </div>
+      </div>
       </div>
     </div>
   );

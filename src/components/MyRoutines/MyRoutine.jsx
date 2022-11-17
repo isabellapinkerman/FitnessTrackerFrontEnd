74135a085bd8218b3d3cd3177253e09a3217320d
Robby Bacus <robbybacus@gmail.com>
2022-11-17 12:17:16 -0500

    organized some things, added error messages for users

--- src/components/MyRoutines/MyRoutine.jsx
+++ src/components/MyRoutines/MyRoutine.jsx
@@ -15,8 +15,9 @@ const MyRoutine = ({
   const [routine, setRoutine] = useState(myRoutine);
 
   const [message, setMessage] = useState(
-    "Please enter routine name and description"
+    "Enter routine name and description."
   );
+
 
   async function handleChangeDeleteRoutine() {
     const deletedRoutine = await deleteRoutine(myRoutine.id, token);
@@ -29,7 +30,7 @@ const MyRoutine = ({
     }
   }
 
-  async function handleSubmitEdit(event) {
+  async function handleSubmitEditRoutine(event) {
     event.preventDefault();
     const name = event.target[0].value;
     const goal = event.target[1].value;
@@ -56,15 +57,18 @@ const MyRoutine = ({
       routine.goal = updatedRoutine.goal;
       routine.isPublic = updatedRoutine.isPublic;
 
-      setRoutine(routine);
-
       setMessage(`You've successfully updated the routine`);
     } else {
       setMessage(`Routine with name "${name}" already exists`);
     }
   }
 
-  async function handleClickSubmit(event) {
+
+  const [attachActivityMessage, setAttachActivityMessage] = useState(
+    "Select an activity then add a duration and count. "
+  );
+  
+  async function handleClickAttachActivity(event) {
     event.preventDefault();
     let routineId = routine.id;
     let activityId = event.target[0].value;
@@ -77,7 +81,13 @@ const MyRoutine = ({
       count,
       duration
     );
-    console.log(allActivities);
+
+    if (!attachedActivity.error) {
+      setMyRoutines(myRoutines);
+      setAttachActivityMessage("Activity attached to routine");
+    } else {
+      setAttachActivityMessage("Activity already exists in routine.");
+    }
   }
 
   return (
@@ -95,75 +105,76 @@ const MyRoutine = ({
           <button onClick={handleChangeDeleteRoutine}>Delete Routine</button>
         </div>
         <div className="editBox">
-        <div className="routineInfo">
-          <div>Routine Edit</div>
-          <div>{message}</div>
-          <form className="registerForm" onSubmit={handleSubmitEdit}>
-            <div>
-              <label htmlFor="name">Name: </label>
-              <input type="text" required></input>
-            </div>
-            <div>
-              <label htmlFor="goal">Goal: </label>
-              <input type="text" required></input>
-            </div>
-            <div>
-              Privacy Setting:
+          <div className="routineInfo">
+            <div>Edit Routine</div>
+            <div>{message}</div>
+            <form className="registerForm" onSubmit={handleSubmitEditRoutine}>
+              <div>
+                <label htmlFor="name">Name: </label>
+                <input type="text" required></input>
+              </div>
+              <div>
+                <label htmlFor="goal">Goal: </label>
+                <input type="text" required></input>
+              </div>
+              <div>
+                Privacy Setting:
+                <select>
+                  <option>Public</option>
+                  <option>Private</option>
+                </select>
+              </div>
+              <div>
+                <button className="button" type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="routineInfo">
+            <div>Attach Activity to Routine</div>
+            <div>{attachActivityMessage}</div>
+            <form onSubmit={handleClickAttachActivity}>
               <select>
-                <option>Public</option>
-                <option>Private</option>
+                {allActivities.map((activity) => {
+                  return (
+                    <option key={`activity-${activity.id}`} value={activity.id}>
+                      {activity.name}
+                    </option>
+                  );
+                })}
               </select>
-            </div>
-            <div>
-              <button className="button" type="submit">
-                Submit
-              </button>
-            </div>
-          </form>
+              <div>
+                <label htmlFor="count">Count: </label>
+                <input type="number" min="0" required></input>
+              </div>
+              <div>
+                <label htmlFor="duration">Duration: </label>
+                <input type="number" min="0" required></input>
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
         </div>
-        <div className="routineInfo">
-          <div>Attach Activity to Routine</div>
-          <form onSubmit={handleClickSubmit}>
-            <select>
-              {allActivities.map((activity) => {
+        <div className="activityBox">
+          <div className="activities">
+            {routine.activities.length ? (
+              routine.activities.map((activity) => {
                 return (
-                  <option key={`activity-${activity.id}`} value={activity.id}>
-                    {activity.name}
-                  </option>
+                  <div className="activity" key={`activity-${activity.id}`}>
+                    <div>{`ID: ${activity.id}`}</div>
+                    <div>{`Name: ${activity.name}`}</div>
+                    <div>{`Description: ${activity.description}`}</div>
+                    <div>{`Duration: ${activity.duration}`}</div>
+                    <div>{`Count: ${activity.count}`}</div>
+                    <div>{`routineActivityId: ${activity.routineActivityId}`}</div>
+                  </div>
                 );
-              })}
-            </select>
-            <div>
-              <label htmlFor="count">Count: </label>
-              <input type="number" min="0" required></input>
-            </div>
-            <div>
-              <label htmlFor="duration">Duration: </label>
-              <input type="number" min="0" required></input>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-</div>
-<div className="activityBox">
-        <div className="activities">
-          {routine.activities.length ? (
-            routine.activities.map((activity) => {
-              return (
-                <div className="activity" key={`activity-${activity.id}`}>
-                  <div>{`ID: ${activity.id}`}</div>
-                  <div>{`Name: ${activity.name}`}</div>
-                  <div>{`Description: ${activity.description}`}</div>
-                  <div>{`Duration: ${activity.duration}`}</div>
-                  <div>{`Count: ${activity.count}`}</div>
-                  <div>{`routineActivityId: ${activity.routineActivityId}`}</div>
-                </div>
-              );
-            })
-          ) : (
-            <div className="noActivity">This routine has no activity.</div>
-          )}
-        </div>
+              })
+            ) : (
+              <div className="noActivity">This routine has no activity.</div>
+            )}
+          </div>
         </div>
       </div>
     </>
